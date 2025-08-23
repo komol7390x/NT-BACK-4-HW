59582a90f8caa5850923a8d0e49ae69affebcea0
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-08-23 12:27:52 +0500

    up

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const bootStrap = async () => {
   const server = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('User create')
+    .setDescription('Users CRUD')
+    .setVersion('1.0')
+    .addTag('user')
+    .build();
+
+  const documentBuilder = () => SwaggerModule.createDocument(server, config);
+  SwaggerModule.setup('api', server, documentBuilder);
+
   const PORT = 3001;
   await server.listen(PORT, () => console.log('Server is running ', PORT));
 };
 
-bootStrap()
+bootStrap();
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -10,21 +10,29 @@ import {
 import { CrateUsersDto } from './dto/create-user.sto';
 import { UpdateUsersDto } from './dto/update-users.dto';
 import { UserService } from './user.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UserController {
   constructor(private readonly UserService: UserService) {}
 
+  @ApiResponse({ status: 201, description: 'Foydalanuvchi yaratildi' })
+  @ApiResponse({ status: 400, description: "Xatov so'rov" })
   @Post()
   create(@Body() CrateUsersDto: CrateUsersDto) {
     return this.UserService.create(CrateUsersDto);
   }
 
+  @ApiResponse({ status: 200, description: 'Barcha foydalanuvchi ' })
+  @ApiResponse({ status: 400, description: "Xatov so'rov" })
   @Get()
   getAll() {
     return this.UserService.getAll();
   }
 
+  @ApiResponse({ status: 200, description: 'Foydalanuvchi' })
+  @ApiResponse({ status: 400, description: "Xatov so'rov" })
   @Get(':id')
   getById(@Param('id') id: string) {
     return this.UserService.getById(id);
