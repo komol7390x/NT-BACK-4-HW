493bc75949f87d441b02a256ed4390bf02591b67
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-08-25 18:07:51 +0500

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,42 +1,10 @@
-import {
-  Module,
-  NestModule,
-  MiddlewareConsumer,
-  RequestMethod,
-} from '@nestjs/common';
-
-import { UserModules } from './users/user.module';
-import { UserMiddleware } from './middleware/user.middleware';
-import { ConfigModule } from '@nestjs/config';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { UserModel } from './users/models/users.entity';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    UserModules,
-    ConfigModule.forRoot({
-      envFilePath:'.env',
-      isGlobal:true
-    }),
-    SequelizeModule.forRoot({
-      dialect:'postgres',
-      host: String(process.env.DB_HOST),
-      port: Number(process.env.DB_PORT),
-      username: String(process.env.DB_USER),
-      password: String(process.env.DB_PASS),
-      database: String(process.env.DB_NAME),
-      logging: false,
-      synchronize: true,
-      autoLoadModels: true,
-      models:[UserModel]
-    })
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(UserMiddleware)
-      .forRoutes({ path: 'users/{*splat}', method: RequestMethod.ALL });
-  }
-}
+export class AppModule {}
