1ee2aaa56feb9b2d930fe596a907d8563b754f31
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-08-27 21:46:55 +0500

    up

--- src/modules/users/saller/dto/create-saller.dto.ts
+++ src/modules/users/saller/dto/create-saller.dto.ts
@@ -1,14 +1,27 @@
 import { IsEmail, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateSallerDto {
+  @ApiProperty({
+    example: 'John Doe',
+    description: 'Sotuvchining to\'liq ismi',
+  })
   @IsString()
   @IsNotEmpty()
   full_name: string;
 
+  @ApiProperty({
+    example: 'johndoe@example.com',
+    description: 'Sotuvchining email manzili',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    example: 30,
+    description: 'Sotuvchining yoshi',
+  })
   @IsNumber()
   @IsNotEmpty()
   age: number;
