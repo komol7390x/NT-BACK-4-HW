ec5d071389832b9ad24280613c303093f72d01f8
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-08-28 16:57:05 +0500

    up

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
@@ -14,6 +15,15 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
   }))
+
+  const config = new DocumentBuilder()
+    .setTitle('Online Course')
+    .setDescription('CRUD online course')
+    .setVersion('1.0')
+    .addTag('course')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const PORT = Number(process.env.PORT) ?? 3000    
   await app.listen(PORT, () => console.log('Server is runing:',PORT));
