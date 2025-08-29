b9100a1f9fe4ccba8a8b87a9cb915d827f486bfa
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-08-29 17:30:32 +0500

    up

--- src/modules/post/order/dto/create-order.dto.ts
+++ src/modules/post/order/dto/create-order.dto.ts
@@ -1,15 +1,28 @@
-import { IsNotEmpty, IsNumber } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateOrderDto {
-    @IsNumber()
-    @IsNotEmpty()
-    quantity:number
+  @ApiProperty({
+    example: 2,
+    description: 'Buyurtma qilinayotgan mahsulot soni',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  quantity: number;
 
-    @IsNumber()
-    @IsNotEmpty()
-    customer_id:number
+  @ApiProperty({
+    example: 1,
+    description: 'Buyurtma qilayotgan customer ID',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  customer_id: number;
 
-    @IsNumber()
-    @IsNotEmpty()
-    product_id:number
+  @ApiProperty({
+    example: 5,
+    description: 'Buyurtma qilinayotgan product ID',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  product_id: number;
 }
